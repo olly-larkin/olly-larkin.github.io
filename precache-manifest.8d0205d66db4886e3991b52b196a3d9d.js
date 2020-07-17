15baced388bd85424f604de5ab444439aaf29cdb
Olly Larkin <olly.larkin@softwire.com>
2020-07-17 17:54:31 +0100

    added the things

---
+++ precache-manifest.8d0205d66db4886e3991b52b196a3d9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1976ea3aa2a280a010eaa505e32e3ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "195e5181e5a246c7d836",
+    "url": "./static/css/2.cee0d1d4.chunk.css"
+  },
+  {
+    "revision": "6c65735c6ce45174c25e",
+    "url": "./static/css/main.1686b9d4.chunk.css"
+  },
+  {
+    "revision": "195e5181e5a246c7d836",
+    "url": "./static/js/2.4c9ee8e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4c9ee8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c65735c6ce45174c25e",
+    "url": "./static/js/main.a9a32a1b.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "cd6d11ef068a3f0d483a61b73044e4ea",
+    "url": "./static/media/defaultProfile.cd6d11ef.png"
+  },
+  {
+    "revision": "4ef8954d49b3a7ab17e7438cb365028b",
+    "url": "./static/media/defaultbanner.4ef8954d.png"
+  }
+]);
